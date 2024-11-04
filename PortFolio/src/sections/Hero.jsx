b6fe51684b7e1e83644f188a6bb6bc8a441c6070
tonkla785 <tonkla785@gmail.com>
2024-11-04 08:27:46 +0700

    Change scale 3D props

--- PortFolio/src/sections/Hero.jsx
+++ PortFolio/src/sections/Hero.jsx
@@ -2,8 +2,57 @@ import { Canvas } from "@react-three/fiber"
 import Something from "../components/Something"
 import { Suspense } from "react"
 import CanvasLoader from "../components/CanvasLoader"
+import { Leva, useControls } from "leva"
 
 const Hero = () => {
+    const { scaleX, scaleY, scaleZ,positionX,positionY,positionZ,rotationX,rotationY ,rotationZ } = useControls("Something", {
+        scaleX: {
+            value: 2.5,
+            min: -10,
+            max: 10,
+        },
+        scaleY: {
+            value: 2.5,
+            min: -10,
+            max: 10,
+        },
+        scaleZ: {
+            value: 2.5,
+            min: -10,
+            max: 10,
+        },
+        positionX: {
+            value: 2.5,
+            min: -10,
+            max: 10,
+        },
+        positionY: {
+            value: 2.5,
+            min: -10,
+            max: 10,
+        },
+        positionZ: {
+            value: 2.5,
+            min: -10,
+            max: 10,
+        },
+        rotationX: {
+            value: 2.5,
+            min: -10,
+            max: 10,
+        },
+        rotationY: {
+            value: 2.5,
+            min: -10,
+            max: 10,
+        },
+        rotationZ: {
+            value: 2.5,
+            min: -10,
+            max: 10,
+        },
+    });
+
   return (
     <section className="min-h-screen w-full flex flex-col relative">
         <div className="w-full mx-auto flex flex-col sm:mt-36 mt-20 c-space gap-3">
@@ -12,11 +61,19 @@ const Hero = () => {
         </div>
 
         <div className="w-full h-full absolute inset-0">
+            <Leva/>
             <Canvas className="w-full h-full">
                 <Suspense fallback={<CanvasLoader/>}>
                 <perspectiveCamera makeDefault position={[0,0,30]}/>
-                <Something scale = {0.05} position = {[0,0,0]} rotation = {[0, Math.PI/2 , 0]}/>
+                <Something 
+                    // scale = {0.6}  
+                    position = {[positionX,positionY,positionZ]} 
+                    rotation = {[rotationX,rotationY ,rotationZ]}
+                    scale ={[scaleX,scaleY,scaleZ]}
+                    />
+                    
                     <ambientLight intensity={1}/>
+                    <directionalLight position={[10,10,10]} intensity={0.5}/>
                 </Suspense>
             </Canvas>
         </div>
